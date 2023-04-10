00c9a95dfb28400f5ce5980daffcbd1b9211b2cd
jared-hwang <jared.hwang@gmail.com>
2023-04-10 16:51:28 +0900

    Add tests for DigitalTyphoonUtils

---
+++ tests/test_DigitalTyphoonUtils.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+
+from DigitalTyphoonDataloader.DigitalTyphoonUtils import *
+
+
+class TestDigitalTyphoonUtils(TestCase):
+
+    def test_parse_image_filename(self):
+        filename = '2008041301-200801-MTS1-1.h5'
+        sequence_num, sequence_datetime, satellite = parse_image_filename(filename)
+        self.assertEqual('200801', sequence_num)
+        self.assertEqual(datetime(2008, 4, 13, 1), sequence_datetime)
+        self.assertEqual('MTS1', satellite)
+
+    def test_get_seq_str_from_track_filename(self):
+        filename = '200801.csv'
+        seq_str = get_seq_str_from_track_filename(filename)
+        self.assertEqual('200801', seq_str)
+
+    def test_is_image_file(self):
+        filename = '200801.csv'
+        self.assertFalse(is_image_file(filename))
+        filename = '2008041302-200801-MTS1-1.h5'
+        self.assertTrue(is_image_file(filename))
+
+    def test_split_unit_has_value(self):
+        self.assertTrue(SPLIT_UNIT.has_value('sequence'))
+        self.assertFalse(SPLIT_UNIT.has_value('nonexistent_value'))
+
+    def test_load_unit_has_value(self):
+        self.assertTrue(LOAD_DATA.has_value('track'))
+        self.assertFalse(LOAD_DATA.has_value('nonexistent_value'))
